b5629dbecc02bd03b619855af9030b733f8cdced
Sergey <f.focus1377@gmail.com>
2023-07-05 15:54:53 +0300

    new project

---
+++ src/users/users.model.ts
@@ -0,0 +1,30 @@
+import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {ApiProperty} from "@nestjs/swagger";
+
+interface UserCreationAttrs {
+    email: string;
+    password: string;
+}
+
+@Table({tableName: 'users'})
+export class User extends Model<User, UserCreationAttrs> {
+    @ApiProperty({example: '1', description: 'Уникальный индентификатор'})
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number;
+
+    @ApiProperty({example: 'user@mail.ru', description: 'Почтовый адрес'})
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    email: string;
+
+    @ApiProperty({example: '12345', description: 'Пароль'})
+    @Column({type: DataType.STRING, allowNull: false})
+    password: string;
+
+    @ApiProperty({example: 'true', description: 'Забанен пользователь или нет'})
+    @Column({type: DataType.BOOLEAN, defaultValue: false})
+    banned: boolean;
+
+    @ApiProperty({example: 'За хулиганство', description: 'Причина бана'})
+    @Column({type: DataType.INTEGER, allowNull: true})
+    banReason: string;
+}
